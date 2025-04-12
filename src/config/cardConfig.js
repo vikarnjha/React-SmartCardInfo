9163c9d102ca953309a2a4b531ac361feaaa5139
Vikarn Jha <vikarnjha91@gmail.com>
2025-04-12 16:45:33 +0530

    card config added

---
+++ src/config/cardConfig.js
@@ -0,0 +1,20 @@
+import {
+    visa,
+    mastercard,
+    rupay,
+    amex,
+    diners,
+    discover,
+    jcb,
+  } from "./cardIcons";
+  
+  export const cardConfig = {
+    visa: { icon: visa, color: "lime" },
+    mastercard: { icon: mastercard, color: "lightblue" },
+    rupay: { icon: rupay, color: "blue" },
+    "american express": { icon: amex, color: "green" },
+    diners: { icon: diners, color: "orange" },
+    discover: { icon: discover, color: "purple" },
+    jcb: { icon: jcb, color: "red" },
+  };
+  
