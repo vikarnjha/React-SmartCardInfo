3ae0353b473c99f0eab1a8bc645bb286f90dc6cf
Vikarn Jha <vikarnjha91@gmail.com>
2025-04-19 16:46:04 +0530

    user model defined

---
+++ backend/models/user.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const userModel = mongoose.models.user || mongoose.model("user", UserSchema);
+
+module.exports = userModel;
