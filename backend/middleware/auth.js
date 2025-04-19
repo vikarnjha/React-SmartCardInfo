26eb337b02ddfe4e78c41eb6feb7e884ea5a0bdf
Vikarn Jha <vikarnjha91@gmail.com>
2025-04-19 17:22:10 +0530

    auth middleware added

---
+++ backend/middleware/auth.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+
+const protectedRoute = (req, res, next) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized: No token provided" });
+  }
+
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = payload.id;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Unauthorized: Invalid token" });
+  }
+};
+
+export { protectedRoute };
