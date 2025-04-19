4eeaa1ce9d24aae28bcc9e4800a7e52b80b86bfd
Vikarn Jha <vikarnjha91@gmail.com>
2025-04-19 16:53:10 +0530

    auth controller boilerplate added

---
+++ backend/controller/auth.controller.js
@@ -0,0 +1,21 @@
+import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
+import User from "../models/User.js"
+
+const register = async (req, res) => {
+    return;
+}
+
+const login = async (req, res) => {
+    return;
+}
+
+const verify = async (req, res) => {
+    return;
+}
+
+const logout = async (req, res) => {
+    return;
+}
+
+export default { register, login, verify, logout }
