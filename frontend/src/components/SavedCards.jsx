ba2fcdfcf9319632f70b54d6de498f541b54bd64
Vikarn Jha <vikarnjha91@gmail.com>
2025-04-22 09:27:57 +0530

    card fetching from database

--- frontend/src/components/SavedCards.jsx
+++ frontend/src/components/SavedCards.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
 import Cards from './Cards'
-import cardsData from '../cards.json'
+// import cardsData from '../cards.json'
 
-const SavedCards = () => {
+const SavedCards = (email) => {
+  const [cards, setCards] = useState([]);
+  useEffect(() => {
+    const fetchCards = async () => {
+      try {
+        const response = await fetch(`http://localhost:5000/api/cards/email/abc@gmail.com`);
+        const data = await response.json();
+        setCards(data);
+      } catch (err) {
+        console.error('Failed to fetch cards', err);
+      }
+    };
+
+    fetchCards();
+  }, [email]);
   return (
     <>
-    {cardsData.map((card, idx) => (
+    {cards.map((card, idx) => (
             <Cards
               key={idx}
               cardNumber={card.cardNumber}
