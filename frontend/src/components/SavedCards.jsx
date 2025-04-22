b797f6487f231e381e77a23439cab6f848bb62ff
Vikarn Jha <vikarnjha91@gmail.com>
2025-04-22 23:44:42 +0530

    fixing path issue

--- frontend/src/components/SavedCards.jsx
+++ frontend/src/components/SavedCards.jsx
@@ -1,17 +1,21 @@
-import {useState, useEffect} from 'react'
-import Cards from './Cards'
+import { useState, useEffect } from "react";
+import Cards from "./Cards";
+import { useAuth } from "../context/AuthContext";
 // import cardsData from '../cards.json'
 
 const SavedCards = (email) => {
   const [cards, setCards] = useState([]);
+  const { user } = useAuth();
   useEffect(() => {
     const fetchCards = async () => {
       try {
-        const response = await fetch(`http://localhost:5000/api/cards/email/abc@gmail.com`);
+        const response = await fetch(
+          `https://react-smartcardinfo.onrender.com/api/cards/email/${user.email}`
+        );
         const data = await response.json();
         setCards(data);
       } catch (err) {
-        console.error('Failed to fetch cards', err);
+        console.error("Failed to fetch cards", err);
       }
     };
 
@@ -19,21 +23,20 @@ const SavedCards = (email) => {
   }, [email]);
   return (
     <>
-    {cards.map((card, idx) => (
-            <Cards
-              key={idx}
-              cardNumber={card.cardNumber}
-              cardExpiry={card.cardExpiry}
-              cardName={card.cardName}
-              cardSecurity={card.cardSecurity}
-              cardType={card.cardType}
-              cardNetwork={card.cardNetwork}
-              cardBrand={card.cardBrand}
-              
-            />
-          ))}
+      {cards.map((card, idx) => (
+        <Cards
+          key={idx}
+          cardNumber={card.cardNumber}
+          cardExpiry={card.cardExpiry}
+          cardName={card.cardName}
+          cardSecurity={card.cardSecurity}
+          cardType={card.cardType}
+          cardNetwork={card.cardNetwork}
+          cardBrand={card.cardBrand}
+        />
+      ))}
     </>
-  )
-}
+  );
+};
 
-export default SavedCards
+export default SavedCards;
