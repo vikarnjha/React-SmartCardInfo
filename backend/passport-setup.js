8897b30474997f1f5b871ab432e8b2eaf296e5af
Vikarn Jha <vikarnjha91@gmail.com>
2025-04-26 16:45:15 +0530

    callback url fixed

--- backend/passport-setup.js
+++ backend/passport-setup.js
@@ -1,6 +1,6 @@
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
-import User from "./models/user.model.js";
+import User from "./models/user.model.js"
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -9,7 +9,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "https://your-domain.com/auth/google/callback",
+      callbackURL: "/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
